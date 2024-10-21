e78660fc1aff2058ded1b22ec4dcfa72ba2898df
u22658395 <u22658395@tuks.co.za>
2024-10-21 19:20:59 +0200

    Added a string identifier for each state

--- BuildingState.h
+++ BuildingState.h
@@ -1,16 +1,20 @@
 #ifndef BUILDINGSTATE_H
 #define BUILDINGSTATE_H
+#include <iostream>
+using namespace std;
 
 class BuildingState
 {
-    private:
-        
+    protected:
+        string name;
     public:
         BuildingState();
         virtual ~BuildingState();
+        virtual string getName();
         virtual BuildingState* demolish(BuildingState* prevState) = 0;
         virtual BuildingState* renovate(BuildingState* prevState) = 0;
         virtual BuildingState* maintenance(BuildingState* prevState) = 0;
 };
 #endif
+
 
