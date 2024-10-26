74536565ff8aa7cb0008a1294c90549d4dcac42c
boitymtsatse <boitymtsatse@gmail.com>
2024-10-26 16:23:51 +0200

    implementation of finance department-constructor-get rest of department from Driya

---
+++ FinanceDept.h
@@ -0,0 +1,11 @@
+#ifndef FinanceDept_H
+#define FinanceDept_H
+
+#include "Department.h"
+
+class FinanceDept : public Department {
+public:
+    FinanceDept(float budget);
+};
+
+#endif // FinanceDept_H
