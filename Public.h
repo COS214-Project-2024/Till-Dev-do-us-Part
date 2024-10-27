48472aef672af81373538ad2b57478140e0c7883
u22658395 <u22658395@tuks.co.za>
2024-10-27 13:08:11 +0200

    Implemented the Public building hierarchy

---
+++ Public.h
@@ -0,0 +1,15 @@
+#ifndef PUBLIC_H
+#define PUBLIC_H
+#include "Building.h"
+
+class Public:public Building
+{
+    public:
+        Public(string type);
+        virtual bool addOccupant(Citizen *c) = 0;
+        virtual void demolish() = 0;             
+        virtual bool clean() = 0;
+        
+};
+#endif
+
