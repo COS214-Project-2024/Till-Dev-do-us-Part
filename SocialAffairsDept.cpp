2fc219965ceded7a43ad8b6d3df3cde01de38622
k-aff <u23543087@tuks.co.za>
2024-10-30 00:03:39 +0200

    Dept using factory and tested for use

--- SocialAffairsDept.cpp
+++ SocialAffairsDept.cpp
@@ -1,44 +1,107 @@
+#include <iostream>
+#include <queue>
+#include <vector>
+#include "Citizen.h"      
+#include "HaveChild.h"
 #include "SocialAffairsDept.h"
 
-//Budget value to be allocated by government 
+using namespace std;
+
 SocialAffairsDept::SocialAffairsDept(float b){
     budget = b;
 }
 
-//Return a citizen to be employed where needed
-Citizen* SocialAffairsDept::employ(Business* employer){
+Citizen* SocialAffairsDept::employ(Business* job) {
 
-    if(!unemployed.empty()){
-        Citizen* employee = unemployed.front();
-        employee->setEmployer(employer);
+    cout << "Entering employ" << endl;
+
+    if (!unemployed.empty()) {
+
+        cout<< "Employing citizen..." << endl;
+        Adult* employee = (Adult*) (unemployed.front());
+        employee->setJob(job);
         unemployed.pop();
-        // cout<<"Successfully employed " << employee.getName() << endl; //Should cout in class that is employing 
-        return employee;
+
+        return (Citizen*) employee;
+    } else {
+        std::cout << "There are no citizens to employ " << std::endl;
+        return nullptr;
     }
 }
 
-void SocialAffairsDept::addCitizen(Citizen* person){
-    // citizens.pushback(person);
-
-    // if(person->employmentStatus)
-
-    //Most effective way to implement??
+void SocialAffairsDept::addCitizen(Citizen* citizen) {
+    adults.push_back(citizen);
+    cout << "Added new adult to adults vector " << endl;
 }
 
-//Get number of citizens
-int SocialAffairsDept::getNumCitizens(){
-    return citizens.size();
+void SocialAffairsDept::addChild(Citizen* citizen) {
+    children.push_back(citizen);
+    cout << "Added new child to children vector " << endl;
 }
 
-//Destruct 
-SocialAffairsDept::~SocialAffairsDept(){
-    for(int i=0; i<unemployed.size();i++){
-        Citizen* employee = unemployed.front();
-        delete employee;
-        unemployed.pop();
+void SocialAffairsDept::addToUnemployed(Citizen* person) {
+    unemployed.push(person);
+    cout << "Added new adult to unemployed queue " << endl;
+}
+
+int SocialAffairsDept::getNumCitizens() {
+    return (adults.size() + children.size());
+}
+
+void SocialAffairsDept::growPopulation(int n) {
+    
+    factory = new AdultPop();
+    Citizen** community = factory->reproduce();
+
+    for (int i = 0; i < n; i++) {
+        addCitizen(community[i]); 
     }
+
+    delete[] community;
+    delete factory;
+
+    factory = new MinorPop();
+
+    int num = 0;
+    auto it = adults.begin();
+
+    for (int i = 0; it != adults.end(); ++it, i+=2) {
+   
+        if ((i % 3) != 0)
+            continue;
+
+        num++;
+        HaveChild* parent = new HaveChild(*it); 
+        Citizen* child = factory->getPerson();
+        parent->haveChild(child); 
+        addChild(child);
+        *it = (Citizen*) parent;
+    }
+
+    delete factory;
+    cout << "Increased current population by 10 adults and " << num << " kids" << endl;
 }
 
-void SocialAffairsDept::growPopulation(){
-    //call on factory to create mmore people
+SocialAffairsDept::~SocialAffairsDept() {
+   
+    while (!unemployed.empty()) {
+        Citizen* employee = unemployed.front();
+        employee = nullptr;  
+        unemployed.pop();  
+    }
+
+    for (auto adult : adults) {
+        delete adult;
+        adult = nullptr;  
+    }
+
+    for (auto child : children) {
+        delete child;
+        child = nullptr;  
+    }
+
+    adults.clear();
+    children.clear();  
+    factory = nullptr;
 }
+
