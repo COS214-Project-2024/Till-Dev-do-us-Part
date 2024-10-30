a8fffe13efa93c829641b3c1ba8d853be07195ae
Driya Govender <u23535793@tuks.co.za>
2024-10-30 19:03:52 +0200

    added karabo's testing to main

--- Main.cpp
+++ Main.cpp
@@ -19,11 +19,21 @@
 #include "WiseBucks.h"
 
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
 using namespace std; 
 
 int main()
 {
-    TaxCalculator* taxes [6]; 
+
+
+    return 0; 
+
+}
+
+void driyasTest()
+{
+        TaxCalculator* taxes [6]; 
     taxes[0] = new CorporateTax(); 
     taxes[1] = new Customs(); 
     taxes[2] = new IncomeTax(); 
@@ -161,7 +171,87 @@ int main()
         delete taxes[i];
         taxes[i] = nullptr; 
     }
+}
 
-    return 0; 
+void karabosTest()
+{
+    cout << "====================================================================================================================================================================================================" << endl;
+
+    std::srand(std::time(0));
+
+    Government* Gov = Government::getInstance();
+    Department* SA = new SocialAffairsDept(50000);
+    Gov->addDepartment("SocialAffairs", SA);
+
+    Population* AdultFactory = new AdultPop();
+    Citizen* personPrototype = AdultFactory->getPerson();
+    Citizen* person = AdultFactory->getPerson();
+    cout << "Created a new adult " << endl;
+
+    Population* MinorFactory = new MinorPop();
+    Citizen* child = MinorFactory->getPerson();
+    cout << "Created a new child " << endl;
+
+    cout << endl;
+
+    person->react();
+    child->react();
+    person->react();
+
+    cout << endl;
+
+    ((SocialAffairsDept*)(Gov->getDepartment("SocialAffairs")))->addCitizen(person);
+    ((SocialAffairsDept*)(Gov->getDepartment("SocialAffairs")))->addChild(child);
+    ((SocialAffairsDept*)(Gov->getDepartment("SocialAffairs")))->addToUnemployed(person);
+    Business* shop = new Business();
+    Citizen* person1 =  ((SocialAffairsDept*)(Gov->getDepartment("SocialAffairs")))->employ(shop);
+
+    cout << endl;
+
+    Citizen* person2 = personPrototype->clone();
+    person2->react();
+
+    cout << endl;
+
+    ((SocialAffairsDept*)(Gov->getDepartment("SocialAffairs")))->growPopulation(10);
+
+    cout << "Currently the city consists of " << ((SocialAffairsDept*)(Gov->getDepartment("SocialAffairs")))->getNumCitizens() << " citizens " << endl;  
+
+    cout << endl;
+
+    if(((Adult*) person1)->employmentStatus())
+    {
+        cout << "Person1 is employed " << endl;
+    }
+
+    if(((Adult*) person)->employmentStatus())
+    {
+        cout << "Person is employed " << endl;
+    }
+
+    delete person2;
+    person2 = nullptr;
+   
+    delete shop;
+    shop = nullptr;
+
+    delete MinorFactory;
+    MinorFactory = nullptr;
+
+    delete personPrototype;
+    personPrototype = nullptr;
+
+    delete AdultFactory;
+    AdultFactory = nullptr;
+
+    Gov->removeDepartment("SocialAffairs");
+
+    delete SA;
+    SA = nullptr;
+
+    delete Gov;
+    Gov = nullptr;
+
+    cout << "====================================================================================================================================================================================================" << endl;
 
 }
