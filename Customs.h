1aeebaff2b69a82b11ded8a847c7b66f68aa0d5d
Driya Govender <u23535793@tuks.co.za>
2024-11-03 18:30:19 +0200

    added doxygen comments to customs

--- Customs.h
+++ Customs.h
@@ -1,3 +1,13 @@
+/**
+ * @file Customs.h
+ * @brief Header file for the Customs class, implementing the TaxCalculator interface.
+ * 
+ * This class represents a concrete strategy for calculating customs taxes, following the Strategy pattern.
+ * It also acts as a command for executing tax calculations specific to customs duties.
+ * 
+ * @author Driya Govender
+ */
+
 /*
     Pattern: Strategy
     Participant: ConcreteStrategy
@@ -11,15 +21,35 @@
 
 #include "TaxCalculator.h"
 
+/**
+ * @class Customs
+ * @brief A concrete implementation of the TaxCalculator for customs tax calculations.
+ * 
+ * This class provides the algorithm to calculate customs tax based on income.
+ */
 class Customs : public TaxCalculator
 {
     public: 
+         /**
+         * @brief Retrieves the type of tax calculation strategy.
+         * 
+         * @return A string indicating the strategy is for customs tax.
+         */
         virtual string getTaxType()
         {
             return "Customs"; 
         }
 
         //strategy function (algorithm)
+        /**
+         * @brief Calculates the customs tax based on the provided income.
+         * 
+         * This function computes the tax amount using a fixed tax rate and
+         * sends the calculated tax to the CRS (Central Revenue Service).
+         * 
+         * @param income The income amount for which the customs tax needs to be calculated.
+         * @return The income amount after the customs tax has been deducted.
+         */
         virtual float calculateTax(float income)
         {
             float taxRate = 0.2; 
