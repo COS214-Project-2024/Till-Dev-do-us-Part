72dac2c7c6c01616e52b80f9ccc8a0e24aff6419
Driya Govender <u23535793@tuks.co.za>
2024-11-03 18:26:56 +0200

    added doxygen comments to corporate tax

--- CorporateTax.h
+++ CorporateTax.h
@@ -1,3 +1,13 @@
+/**
+ * @file CorporateTax.h
+ * @brief Header file for the CorporateTax class, implementing the TaxCalculator interface.
+ * 
+ * This class represents a concrete strategy for calculating corporate taxes, following the Strategy pattern.
+ * It also acts as a command for executing tax calculations specific to corporate income.
+ * 
+ * @author Driya Govender
+ */
+
 /*
     Pattern: Strategy
     Participant: ConcreteStrategy
@@ -11,15 +21,35 @@
 
 #include "TaxCalculator.h"
 
+/**
+ * @class CorporateTax
+ * @brief A concrete implementation of the TaxCalculator for corporate tax calculations.
+ * 
+ * This class provides the algorithm to calculate corporate tax based on income.
+ */
 class CorporateTax : public TaxCalculator
 {
     public: 
+        /**
+         * @brief Retrieves the type of tax calculation strategy.
+         * 
+         * @return A string indicating the strategy is for corporate tax.
+         */
         virtual string getTaxType()
         {
             return "Corporate Tax"; 
         }
         
         //strategy function (algorithm)
+        /**
+         * @brief Calculates the corporate tax based on the provided income.
+         * 
+         * This function computes the tax amount using a fixed tax rate and
+         * sends the calculated tax to the CRS (Central Revenue Service).
+         * 
+         * @param income The income amount for which the corporate tax needs to be calculated.
+         * @return The income amount after the corporate tax has been deducted.
+         */
         virtual float calculateTax(float income)
         {
             float taxRate = 0.27; 
