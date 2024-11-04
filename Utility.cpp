977320201665e1ac2eaf6bdcf65ebe2ee1b0000d
u23772141 <u23772141@tuks.co.za>
2024-11-04 13:41:02 +0200

    Utility Doxygen comments

--- Utility.cpp
+++ Utility.cpp
@@ -1,47 +1,80 @@
+/**
+ * @file Utility.cpp
+ * @brief Implements the Utility class methods for managing utilities in the simulation.
+ * @author rethabile
+ */
+
 #include "Utility.h"
 #include <iostream>
 
+/**
+ * @brief Constructs a Utility with specified name, Resource Department, resource, and maximum workers.
+ *        Initializes a Wellness business for the utility.
+ * @param name Name of the utility
+ * @param resDept Pointer to the Resource Department
+ * @param resource Pointer to the resources used by the utility
+ * @param maxWorkers Maximum number of workers for the utility
+ */
 Utility::Utility(const std::string &name, ResourceDepartment *resDept, Resources *resource, int maxWorkers)
     : name(name), resDept(resDept), resource(resource), MaxWorkers(maxWorkers)
 {
-    std::cout<<"segfault"<<std::endl;
     UtilityBus = new Wellness("Utility", MaxWorkers);
     std::cout << "Buying Property for Utilities" << std::endl;
     UtilityBus->buyProperty("Plant");
-    std::cout << "Hiring Employess for Utilities" << std::endl;
+    std::cout << "Hiring Employees for Utilities" << std::endl;
+
     for (int i = 0; i <= MaxWorkers; i++)
     {
         UtilityBus->hireEmployee();
     }
+    ActiveWorkers = UtilityBus->activelyWorking();
 }
 
+/**
+ * @brief Default destructor for Utility.
+ */
 Utility::~Utility() = default;
 
+/**
+ * @brief Sends a notification message to the Resource Department.
+ * @param message The message to be sent to the Resource Department
+ */
 void Utility::notifyResourceDept(const std::string &message)
 {
-    // Notify the Resource Department with a specific message
     std::cout << "Notifying Resource Department: " << message << std::endl;
 }
 
+/**
+ * @brief Gets the name of the utility.
+ * @return The name of the utility
+ */
 std::string Utility::getName() const
 {
     return name;
 }
 
+/**
+ * @brief Gets the resource pointer used by the utility.
+ * @return Pointer to the resources used by the utility
+ */
 Resources *Utility::getResource() const
 {
     return resource;
 }
 
-void Utility::getStatus() const
-{
-    std::cout << "---- Utility Status ----" << std::endl;
-}
-
+/**
+ * @brief Gets the number of currently active workers.
+ * @return The number of active workers
+ */
 int Utility::getActiveWorkers() const
 {
     return ActiveWorkers;
 }
+
+/**
+ * @brief Gets the maximum number of workers allowed for the utility.
+ * @return The maximum number of workers
+ */
 int Utility::getMaxWorkers() const
 {
     return MaxWorkers;
--- Utility.h
+++ Utility.h
@@ -1,39 +1,93 @@
+/**
+ * @file Utility.h
+ * @brief Defines the Utility class used in managing utilities in the simulation.
+ * @author rethabile
+ */
+
 #ifndef UTILITY_H
 #define UTILITY_H
 
 #include <string>
-#include "ResourceDepartment.h"
 #include "Resources.h"
 #include "Business.h"
 #include "Wellness.h"
 
 class ResourceDepartment;
+
+/**
+ * @class Utility
+ * @brief Abstract base class for managing various utilities in the simulation.
+ */
 class Utility
 {
 protected:
-    std::string name;
-    ResourceDepartment *resDept; // Pointer to the Resource Department
-    Resources *resource;         // Pointer to the resources used by the utility
-    Business * UtilityBus;//Hire workers using Business
-    int MaxWorkers;
-    int ActiveWorkers;
+    std::string name;                  ///< Name of the utility
+    ResourceDepartment *resDept;       ///< Pointer to the Resource Department
+    Resources *resource;               ///< Pointer to the resources used by the utility
+    Business *UtilityBus;              ///< Manages worker hiring for the utility
+    int MaxWorkers;                    ///< Maximum number of workers the utility can hire
+    int ActiveWorkers;                 ///< Currently active workers
 
 public:
-    // Constructors and destructor
+    /**
+     * @brief Default constructor for Utility.
+     */
     Utility();
-    Utility(const std::string &name, ResourceDepartment *resDept, Resources *resource,int maxWorkers);
+
+    /**
+     * @brief Parameterized constructor for Utility.
+     * @param name Name of the utility
+     * @param resDept Pointer to the Resource Department
+     * @param resource Pointer to the resources used by the utility
+     * @param maxWorkers Maximum number of workers for the utility
+     */
+    Utility(const std::string &name, ResourceDepartment *resDept, Resources *resource, int maxWorkers);
+
+    /**
+     * @brief Virtual destructor for Utility.
+     */
     virtual ~Utility();
 
-  
+    /**
+     * @brief Sends a notification message to the Resource Department.
+     * @param message The message to be sent to the Resource Department
+     */
     void notifyResourceDept(const std::string &message);
 
-    // Getters and setters
+    /**
+     * @brief Gets the name of the utility.
+     * @return The name of the utility
+     */
     virtual std::string getName() const;
+
+    /**
+     * @brief Pure virtual function to get the type of utility.
+     * @return The type of utility
+     */
+    virtual std::string getType() const = 0;
+
+    /**
+     * @brief Pure virtual function to get the status of the utility.
+     */
+    virtual void getStatus() const = 0;
+
+    /**
+     * @brief Gets the number of active workers.
+     * @return The number of active workers
+     */
     int getActiveWorkers() const;
+
+    /**
+     * @brief Gets the maximum number of workers.
+     * @return The maximum number of workers
+     */
     int getMaxWorkers() const;
-    virtual void getStatus() const;
+
+    /**
+     * @brief Gets the resource pointer used by the utility.
+     * @return Pointer to the resources used by the utility
+     */
     Resources *getResource() const;
-    
 };
 
 #endif // UTILITY_H
